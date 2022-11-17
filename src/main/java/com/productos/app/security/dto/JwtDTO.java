2b09497dd72e494640e19ad55d68cadf95cf6720
Capdoo <mazher_2014@hotmail.com>
2022-11-17 10:42:06 -0500

    jwt ok

--- src/main/java/com/productos/app/security/dto/JwtDTO.java
+++ src/main/java/com/productos/app/security/dto/JwtDTO.java
@@ -1,23 +1,15 @@
 package com.productos.app.security.dto;
 
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.Collection;
-
 public class JwtDTO {
 
 	private String token;
-	private String bearer = "Bearer";
-	private String nombreUsuario;
-	
-	//Collections: De authorities
-	private Collection<? extends GrantedAuthority> authorities;
 
-	public JwtDTO(String token, String nombreUsuario, Collection<? extends GrantedAuthority> authorities) {
+	public JwtDTO() {
+	}
+
+	public JwtDTO(String token) {
 		super();
 		this.token = token;
-		this.nombreUsuario = nombreUsuario;
-		this.authorities = authorities;
 	}
 
 	public String getToken() {
@@ -28,29 +20,5 @@ public class JwtDTO {
 		this.token = token;
 	}
 
-	public String getBearer() {
-		return bearer;
-	}
-
-	public void setBearer(String bearer) {
-		this.bearer = bearer;
-	}
-
-	public String getNombreUsuario() {
-		return nombreUsuario;
-	}
-
-	public void setNombreUsuario(String nombreUsuario) {
-		this.nombreUsuario = nombreUsuario;
-	}
-
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return authorities;
-	}
-
-	public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
-		this.authorities = authorities;
-	}
-	
 	
 }
--- src/main/java/com/productos/app/security/jwt/JwtProvider.java
+++ src/main/java/com/productos/app/security/jwt/JwtProvider.java
@@ -6,9 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 //Generar token : validación en la creación
 
@@ -25,24 +28,30 @@ public class JwtProvider {
 	
 	public String generateToken(Authentication authentication) {
 		UsuarioPrincipalModel usuarioPrincipal = (UsuarioPrincipalModel) authentication.getPrincipal();
-		
-		return Jwts.builder().setSubject(usuarioPrincipal.getUsername())
+		//Nuevo
+		List<String> roles = usuarioPrincipal.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(
+				Collectors.toList()
+		);
+
+		return Jwts.builder()
+				.setSubject(usuarioPrincipal.getUsername())
+				.claim("roles", roles)
 				.setIssuedAt(new Date())
 				.setExpiration(new Date(new Date().getTime() + expiration*1000))
-				.signWith(SignatureAlgorithm.HS512, secret)
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
 	}
 	
 	//Obtiene nombreUsuario desde token
 	public String getNombreUsuarioFromToken(String token) {
-		return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+		return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody().getSubject();
 	}
 	
 	
 	public boolean validateToken(String token) {
 		try {
 			
-			Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+			Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token);
 			return true;
 			
 		} catch (MalformedJwtException e) {
